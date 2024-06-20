60ae2a7e050eaafaa86fcb1b60032a007638a5c0
Channuch <channu.xworkz@gmail.com>
2024-06-20 14:28:31 +0530

    Add files via upload

---
+++ DMart.java
@@ -0,0 +1,157 @@
+class DMart{
+
+static String chocolates[] = {"1.kit kat ","2. Dairy Milk ","3. Snickers ","4. Munch ","5. Milky Bar ", "6. Silk ","7. 5 Star "};
+static String vegitables[] = {"1.Tomato","2.Potato","3.Ladyfinger","4.Brinjal","5.Capsicum","6.Carrot","7.Cucumber","8.Onion","9.Ginger"};
+static String froots[] = {"1.Apple","2. Orange","3.Grapes","4.Watermelon","5.Pineapple","6.Mango","7.Dragonfroot","8.Cherry"};
+static String coolDrinks[] = {"1.Sprite","2.Coco Cola","3.Fanta","4.Pepsi","5.Maza","6.Limca","7.Appy Fizz","8.Mirinda"};
+static String groceries[] = {"1. Dalls","2. Atta","3. Oils","4. Salt","5. Sugure","6. Rice","7. Teapowder","8. Milk"};
+static String homeAppliances[] = {"1.Fan","2.Tv","3.Sofa","4.Bed","5.Blender","6.WashingMachine","7.Vacume Cleaner","8. DishWasher"};
+static String clothes[] = {"1.Shirts","2.Pants","3.T-shirts","4.Shorts","5.Nightpants"};
+static String footWare[] = {"1.Sneakers","2.Casual Shoes","3.Sport Shoes","4.Sandles","5.Sleepers","6.Crocs","7.Loofers"};
+static String stationery[] = {"1.Pens and Pencils","2.Notebooks and Notepads","3.Filing and Organization","4.Paper Products","5.Art Supplies","6.Desk Accessories","7.Correction Supplies","8.Writing Instruments"};
+static String personalCare[] = {"1.Shampoo and Conditioner","2.Body Wash and Soap","3.Skincare","4.Oral Care","5.Hair Care Accessories","6.Cosmetics","7.Men's Grooming","8.Sunscreen and Sun Care"};
+static String electronics[] = {"1.Batteries","2.EarPhones","3.Cables","4.Torch","5.Chargers","6.clocks"};
+static String homeDecor[] = {"1.Curtains and Drapes","2.Rugs and Carpets","3.Cushions and Throws","4.Wall Art","5.Mirrors","6.Photo Frames","7.Candles and Candle Holders","8.Vases and Artificial Flowers"};
+static String toysAndGames[] = {"1.Carrom","2.Chess","3.Bat","4.Ball","5.Basket Ball","6.Foot Ball","7.BadMinton","8.Sket Board"};
+static String biscuits[] = {"1.Parle-G","2.BorBone","3.MaryGold","4.Tiger","5.Goodday","6.Happy Happy","7.Hide And Seak","8.Oreo"};
+static String packetsFoods[] = {"1.Noodles","2.Maggie","3.CUP Noodles","4.Soups","5.PoP Corns"};
+
+
+
+
+public static void main(String newCollection[]){
+
+System.out.println("The Available Chocolate Are : \n "
++ chocolates[0]+"\n"+
+chocolates[1]+"\n"+
+chocolates[2]+"\n"+
+chocolates[3]+"\n"+
+chocolates[4]+"\n"+
+chocolates[5]+"\n"+
+chocolates[6]+"\n");
+System.out.println("The Available Fresh Vegitables Are : \n "
++ vegitables[0]+"\n"+
+vegitables[1]+"\n"+
+vegitables[2]+"\n"+
+vegitables[3]+"\n"+
+vegitables[4]+"\n"+
+vegitables[5]+"\n"+
+vegitables[6]+"\n"+
+vegitables[7]+"\n"+
+vegitables[8]+"\n");
+System.out.println("The Available Fresh Froots Are : \n "
++ froots[0]+"\n"+
+froots[1]+"\n"+
+froots[2]+"\n"+
+froots[3]+"\n"+
+froots[4]+"\n"+
+froots[5]+"\n"+
+froots[6]+"\n"+
+froots[7]+"\n");
+System.out.println("The Available CoolDrinks Are : \n "
++ coolDrinks[0]+"\n"+
+coolDrinks[1]+"\n"+
+coolDrinks[2]+"\n"+
+coolDrinks[3]+"\n"+
+coolDrinks[4]+"\n"+
+coolDrinks[5]+"\n"+
+coolDrinks[6]+"\n"+
+coolDrinks[7]+"\n");
+System.out.println("The Available Groceries Are : \n "
++ groceries[0]+"\n"+
+groceries[1]+"\n"+
+groceries[2]+"\n"+
+groceries[3]+"\n"+
+groceries[4]+"\n"+
+groceries[5]+"\n"+
+groceries[6]+"\n"+
+groceries[7]+"\n");
+System.out.println("The Home Appliances : \n "
++homeAppliances[0]+"\n"+
+homeAppliances[1]+"\n"+
+homeAppliances[2]+"\n"+
+homeAppliances[3]+"\n"+
+homeAppliances[4]+"\n"+
+homeAppliances[5]+"\n"+
+homeAppliances[6]+"\n"+
+homeAppliances[7]+"\n");
+System.out.println("The New Arrived Clothes : \n "
++clothes[0]+"\n"+
+clothes[1]+"\n"+
+clothes[2]+"\n"+
+clothes[3]+"\n"+
+clothes[4]+"\n");
+System.out.println("The New Footware Collection : \n "
++ footWare[0]+"\n"+
+footWare[1]+"\n"+
+footWare[2]+"\n"+
+footWare[3]+"\n"+
+footWare[4]+"\n"+
+footWare[5]+"\n"+
+footWare[6]+"\n");
+System.out.println("The Available Stationery Products: \n "
++ stationery[0]+"\n"+
+stationery[1]+"\n"+
+stationery[2]+"\n"+
+stationery[3]+"\n"+
+stationery[4]+"\n"+
+stationery[5]+"\n"+
+stationery[6]+"\n"+
+stationery[7]+"\n");
+System.out.println("The Available Personal Care Products : \n "
++ personalCare[0]+"\n"+
+personalCare[1]+"\n"+
+personalCare[2]+"\n"+
+personalCare[3]+"\n"+
+personalCare[4]+"\n"+
+personalCare[5]+"\n"+
+personalCare[6]+"\n"+
+personalCare[7]+"\n");
+System.out.println("The Available Electronics Are : \n "
++ electronics[0]+"\n"+
+electronics[1]+"\n"+
+electronics[2]+"\n"+
+electronics[3]+"\n"+
+electronics[4]+"\n"+
+electronics[5]+"\n");
+System.out.println("The Available Home Decors Are : \n "
++ homeDecor[0]+"\n"+
+homeDecor[1]+"\n"+
+homeDecor[2]+"\n"+
+homeDecor[3]+"\n"+
+homeDecor[4]+"\n"+
+homeDecor[5]+"\n"+
+homeDecor[6]+"\n"+
+homeDecor[7]+"\n");
+System.out.println("The Available Toys And Games Are : \n "
++ toysAndGames[0]+"\n"+
+toysAndGames[1]+"\n"+
+toysAndGames[2]+"\n"+
+toysAndGames[3]+"\n"+
+toysAndGames[4]+"\n"+
+toysAndGames[5]+"\n"+
+toysAndGames[6]+"\n"+
+toysAndGames[7]+"\n");
+System.out.println("The Available Biscuits : \n "
++ biscuits[0]+"\n"+
+biscuits[1]+"\n"+
+biscuits[2]+"\n"+
+biscuits[3]+"\n"+
+biscuits[4]+"\n"+
+biscuits[5]+"\n"+
+biscuits[6]+"\n"+
+biscuits[7]+"\n");
+System.out.println("The Available Packet Foods : \n "
++ packetsFoods[0]+"\n"+
+packetsFoods[1]+"\n"+
+packetsFoods[2]+"\n"+
+packetsFoods[3]+"\n"+
+packetsFoods[4]+"\n");
+
+
+
+
+
+}
+
+}
